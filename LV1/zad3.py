b49f7538ef793e7e4ba89fbeebc4ff6519a87b75
Shoosht <115585203+Shoosht@users.noreply.github.com>
2023-04-07 22:27:42 +0200

    Update zad3.py

--- LV1/zad3.py
+++ LV1/zad3.py
@@ -1,7 +1,11 @@
 import cryptography
-
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+
 private_key = rsa.generate_private_key(
     public_exponent=65537,
     key_size=2048,
@@ -10,23 +14,15 @@ private_key = rsa.generate_private_key(
 public_key = private_key.public_key()
 
 
-
-from cryptography.hazmat.primitives import serialization
-
 pem = private_key.private_bytes(
     encoding=serialization.Encoding.PEM,
     format=serialization.PrivateFormat.PKCS8,
     encryption_algorithm=serialization.NoEncryption()
 )
 
+
 with open('private_key.pem', 'wb') as f:
     f.write(pem)
-
-
-
-
-
-
 
 
 pem = public_key.public_bytes(
@@ -34,13 +30,9 @@ pem = public_key.public_bytes(
     format=serialization.PublicFormat.SubjectPublicKeyInfo
 )
 
+
 with open('public_key.pem', 'wb') as f:
     f.write(pem)
-
-
-
-
-
 
 
 with open("private_key.pem", "rb") as key_file:
@@ -51,23 +43,16 @@ with open("private_key.pem", "rb") as key_file:
     )
 
 
-
-
-
-
 with open("public_key.pem", "rb") as key_file:
     public_key = serialization.load_pem_public_key(
         key_file.read(),
         backend=default_backend()
     )
 
-
-
-
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import padding
+    
 
 message = b'Message'
+
 
 
 encrypted = public_key.encrypt(
@@ -80,14 +65,6 @@ encrypted = public_key.encrypt(
 )
 
 
-
-
-
-
-
-
-#private_key = ... # Use one of the methods above to get your public key (matches the public_key)
-
 original_message = private_key.decrypt(
     encrypted,
     padding.OAEP(
@@ -96,6 +73,8 @@ original_message = private_key.decrypt(
         label=None
     )
 )
+
+
 
 temp = original_message.decode()
 print("Original message = " + temp)
